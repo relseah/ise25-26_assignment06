f0c39c53fbb2886cb17f45220e8fb246cdb8cd8b
David Häsler <haesler.da@gmail.com>
2025-12-02 07:12:47 +0100

    Implement UserController

--- api/src/main/java/de/seuhd/campuscoffee/api/controller/UserController.java
+++ api/src/main/java/de/seuhd/campuscoffee/api/controller/UserController.java
@@ -1,10 +1,25 @@
 package de.seuhd.campuscoffee.api.controller;
 
+import de.seuhd.campuscoffee.api.dtos.UserDto;
+import de.seuhd.campuscoffee.api.exceptions.ErrorResponse;
+import de.seuhd.campuscoffee.api.mapper.UserDtoMapper;
+import de.seuhd.campuscoffee.domain.model.User;
+import de.seuhd.campuscoffee.domain.ports.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static de.seuhd.campuscoffee.api.util.ControllerUtils.getLocation;
 
 @Tag(name = "Users", description = "Operations related to user management.")
 @Controller
@@ -12,5 +27,200 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RequiredArgsConstructor
 public class UserController {
-    //TODO: Implement user controller
+    private final UserService userService;
+    private final UserDtoMapper userDtoMapper;
+
+    @Operation(
+            summary = "Get all users.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(type = "array", implementation = UserDto.class)
+                            ),
+                            description = "All users as a JSON array."
+                    )
+            }
+    )
+    @GetMapping("")
+    public ResponseEntity<List<UserDto>> getAll() {
+
+        return ResponseEntity.ok(
+                userService.getAll().stream()
+                        .map(userDtoMapper::fromDomain)
+                        .toList()
+        );
+    }
+
+    @Operation(
+            summary = "Get user by ID.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = User.class)
+                            ),
+                            description = "The user with the provided ID as a JSON object."
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            ),
+                            description = "No user with the provided ID could be found."
+                    )
+            }
+    )
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getById(
+            @PathVariable Long id) {
+
+        return ResponseEntity.ok(
+                userDtoMapper.fromDomain(userService.getById(id))
+        );
+    }
+
+    @Operation(
+            summary = "Get user by login name.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = UserDto.class)
+                            ),
+                            description = "The user with the provided login name as a JSON object."
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            ),
+                            description = "No user with the provided login name could be found."
+                    )
+            }
+    )
+    @GetMapping("/filter")
+    public ResponseEntity<UserDto> filter(
+            @RequestParam("login_name") String loginName) {
+
+        return ResponseEntity.ok(
+                userDtoMapper.fromDomain(userService.getByLoginName(loginName))
+        );
+    }
+
+    @Operation(
+            summary = "Create a new user.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = UserDto.class)
+                            ),
+                            description = "The new user as a JSON object."
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            ),
+                            description = "Validation failed."
+                    )
+            }
+    )
+    @PostMapping("")
+    public ResponseEntity<UserDto> create(
+            @RequestBody @Valid UserDto userDto) {
+
+        UserDto created = upsert(userDto);
+        return ResponseEntity
+                .created(getLocation(created.id()))
+                .body(created);
+    }
+
+    @Operation(
+            summary = "Update an existing user by ID.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = UserDto.class)
+                            ),
+                            description = "The updated user as a JSON object."
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            ),
+                            description = "Validation failed."
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            ),
+                            description = "No user with the provided ID could be found."
+                    )
+            }
+    )
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDto> update(
+            @PathVariable Long id,
+            @RequestBody @Valid UserDto userDto) {
+
+        if (!id.equals(userDto.id())) {
+            throw new IllegalArgumentException("User ID in path and body do not match.");
+        }
+        return ResponseEntity.ok(
+                upsert(userDto)
+        );
+    }
+
+    @Operation(
+            summary = "Delete a user by ID.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "The user was successfully deleted."
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            ),
+                            description = "No user with the provided ID could be found."
+                    )
+            }
+    )
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(
+            @PathVariable Long id) {
+        userService.delete(id); // throws NotFoundException if no user with the provided ID exists
+        return ResponseEntity.noContent().build();
+    }
+
+    /**
+     * Common upsert logic for create and update.
+     *
+     * @param userDto the user DTO to map and upsert
+     * @return the upserted user mapped back to the DTO format.
+     */
+    private UserDto upsert(UserDto userDto) {
+        return userDtoMapper.fromDomain(
+                userService.upsert(
+                        userDtoMapper.toDomain(userDto)
+                )
+        );
+    }
 }
