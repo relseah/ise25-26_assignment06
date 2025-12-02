f7d6ef652329a8645db940109044db05d181642c
David Häsler <haesler.da@gmail.com>
2025-12-02 06:25:34 +0100

    Implement User in domain module and UserDtoMapper

--- domain/src/main/java/de/seuhd/campuscoffee/domain/model/User.java
+++ domain/src/main/java/de/seuhd/campuscoffee/domain/model/User.java
@@ -1,13 +1,22 @@
 package de.seuhd.campuscoffee.domain.model;
 
 import lombok.Builder;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Builder(toBuilder = true)
 public record User (
-        //TODO: Implement user domain object
+        @Nullable Long id,
+        @Nullable LocalDateTime createdAt,
+        @Nullable LocalDateTime updatedAt,
+        @NonNull String loginName,
+        @NonNull String emailAddress,
+        @NonNull String firstName,
+        @NonNull String lastName
 ) implements Serializable { // serializable to allow cloning (see TestFixtures class).
     @Serial
     private static final long serialVersionUID = 1L;
